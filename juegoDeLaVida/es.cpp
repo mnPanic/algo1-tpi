9222cd9b28ce632f6830680301e88b1b7453fe97
mnPanic <panicmanu@gmail.com>
2018-10-31 23:26:45 -0300

    agrego formato del archivo a ES

--- juegoDeLaVida/es.cpp
+++ juegoDeLaVida/es.cpp
@@ -1,6 +1,19 @@
 #include "es.h"
 #include "definiciones.h"
 
+/* Formato del archivo
+ * 1. Cantidad de filas (cantidadFilas).
+ *
+ * 2. Cantidad de columnas (cantidadColumnas).
+ *
+ * 3. Lista de números que indican los valores de cada celda, ordenados primero por fila, y luego por columna.
+ *      - 1 indica que la celda está viva,
+ *      - 0 que está muerta.
+ *    Debe haber exactamente cantidadFilas × cantidadColumnas valores.
+ *
+ * 4. Entero positivo que se debe corresponder a la cantidad de celdas vivas que tiene el toroide
+ */
+
 /******************************** EJERCICIO cargarToroide *******************************/
 toroide cargarToroide(string nombreArchivo, bool &status)
 {
