15b062a78cb3c66b907115b29c14ada7ca911927
mnPanic <panicmanu@gmail.com>
2018-10-31 23:50:32 -0300

    ES: agrego especificación de cargarToroide

--- juegoDeLaVida/es.cpp
+++ juegoDeLaVida/es.cpp
@@ -15,8 +15,19 @@
  */
 
 /******************************** EJERCICIO cargarToroide *******************************/
-toroide cargarToroide(string nombreArchivo, bool &status)
-{
+
+/**
+ * Dado un nombre de archivo, lee e interpreta el contenido del mismo como un toroide.
+ * Modifica status para indicar si el procesamiento fue exitoso o no.
+ * Los motivos por los que status = false (no exitoso) son:
+ *   - No se pudo abrir el archivo (inexistente o sin permisos).
+ * 	 - Los datos contenidos en el archivo no respetan el formato descripto anteriormente.
+ *
+ * @param nombreArchivo El nombre del archivo.
+ * @param status El estado. Indica si fue exitoso.
+ * @return El toroide cargado.
+ */
+toroide cargarToroide(string nombreArchivo, bool &status) {
 	toroide t;
 	return t;
 }
