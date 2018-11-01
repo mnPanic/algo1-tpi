3e2022f076c49b4fec1682e0025cf9cddee252fb
mnPanic <panicmanu@gmail.com>
2018-11-01 00:51:20 -0300

    ES: WIP cargarToroide

--- juegoDeLaVida/es.cpp
+++ juegoDeLaVida/es.cpp
@@ -1,5 +1,8 @@
 #include "es.h"
 #include "definiciones.h"
+#include <fstream>
+
+namespace std
 
 /* Formato del archivo
  * 1. Cantidad de filas (cantidadFilas).
@@ -29,7 +32,83 @@
  */
 toroide cargarToroide(string nombreArchivo, bool &status) {
 	toroide t;
+
+	if (esArchivoValido(nombreArchivo) && esFormatoValido(nombreArchivo)) {
+		t = leerToroide(nombreArchivo);
+	} else {
+		status = false;
+	}
+
 	return t;
+}
+
+private bool esArchivoValido(String nombreArchivo) {
+	ifstream fin(nombreArchivo);
+
+	return fin.fail()
+}
+
+
+/*
+ *
+5 4
+
+ 1 1
+ 1 0
+ 1 0
+ 0 0
+ 0 0
+ 0 0
+ 0 0
+ 0 0
+ 0 0
+
+1 0 0 0
+0 0 1 0
+0 0 0 1
+0 1 0 0
+0 0 0 0
+4
+ */
+
+// supone que es un archivo válido
+private bool esFormatoValido(String nombreArchivo) {
+	bool valido = true;
+
+	ifstream fin(nombreArchivo);
+
+	int filasEsperadas = 0;
+	int columnasEsperadas = 0;
+	int cantVivasEsperadas = -1;	// comienza en -1...
+
+	fin >> filasEsperadas;
+	fin >> columnasEsperadas;
+
+	int filas = 0;
+	int columnas = 0;
+	int vivas = 0;
+
+	int valor = 0;
+
+	for (int i = 0; i < filasEsperadas * columnasEsperadas && !fin.eof(); i ++) {
+		// problema: no diferenciamos entre dimensiones para misma cantidad de elementos
+		fin >> valor;
+
+		if (valor == 1 || valor == 0) {
+			vivas += valor;
+		} else {
+			valido = false;
+		}
+	}
+
+	if (!fin.eof()) {
+		fin >> cantVivasEsperadas;
+		// deberñia ser true fin.eof()
+	}
+
+	// falta chequear que cantVivasEsperadas sea correcto
+	// y que haya solo 0 y 1
+	return fin.eof() && filas == filasEsperadas && columnas == columnasEsperadas;
 }
 
 
