792a84b1dda30d12ab20052929b058be56ec0144
mnPanic <panicmanu@gmail.com>
2018-11-04 17:33:50 -0300

    evolucionDePosicion: agrego tests para mod

---
+++ juegoDeLaVida/tests/nuestros/modTEST.cpp
@@ -0,0 +1,24 @@
+#include "../../auxiliares.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+TEST(modTEST, nPositivoMayorAM){
+    EXPECT_EQ(mod(14, 5), 4);
+}
+
+TEST(modTEST, nPositivoDivisiblePorM){
+    EXPECT_EQ(mod(15, 5), 0);
+}
+
+TEST(modTEST, nPositivoMenorAM){
+    EXPECT_EQ(mod(2, 5), 2);
+}
+
+TEST(modTEST, nNegativo){
+    EXPECT_EQ(mod(-2, 5), 3);
+}
+
+TEST(modTEST, nCero){
+    EXPECT_EQ(mod(0, 5), 0);
+}
