a7b0d5a74192a0f99d3e4ce87d53bea885b3e5f5
mnPanic <panicmanu@gmail.com>
2018-11-14 23:46:14 -0300

    primosLejanos: agrego test para evolucion no inmediata

--- juegoDeLaVida/tests/nuestros/primosLejanos/primosLejanosTEST.cpp
+++ juegoDeLaVida/tests/nuestros/primosLejanos/primosLejanosTEST.cpp
@@ -12,9 +12,8 @@ TEST(primosLejanosTEST, toroidesIgualesPeriodicos) {
     };
 
     EXPECT_TRUE(primosLejanos(t, t));
-
-
 }
+
 TEST(primosLejanosTEST, toroidesIgualesNoPeriodicos) {
     toroide t = {
             {false, false, false},
@@ -23,6 +22,22 @@ TEST(primosLejanosTEST, toroidesIgualesNoPeriodicos) {
     };
 
     EXPECT_TRUE(primosLejanos(t, t));
+}
 
+TEST(primosLejanosTEST, evolucionNoInmediata) {
+    toroide t1 = {
+            {false, true,  false, false},
+            {false, true,  true,  false},
+            {false, false, true,  false},
+            {false, false, false, false}
+    };
 
+    toroide t2 = {
+            {true,  true,   true,  true},
+            {true,  false,  false, true},
+            {true,  true,   true,  true},
+            {false, false, false, false}
+    };
+
+    EXPECT_TRUE(primosLejanos(t1, t2));
 }
